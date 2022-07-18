a972be660c68e360cba7e6c9c5987512577249b3
pensir04 <97670299+pensir04@users.noreply.github.com>
2022-07-18 19:11:40 +1000

    Delete 7.6.test.js

--- 7.6.test.js
+++
@@ -1,8 +0,0 @@
-import { reverseString } from "7.6.js";
-
-describe("reverse for string", () => {
-  it("should get reverse string", () => {
-    const result = 'привет';
-    expect(result).toBe('тевирп');
-  });
-});
